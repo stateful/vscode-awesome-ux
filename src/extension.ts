e5be5e861efae3960c971523acb2f05f58ca16ab
Christian Bromann <git@bromann.dev>
2021-11-30 14:44:20 +0100

    add editorconfig file

--- src/extension.ts
+++ src/extension.ts
@@ -10,24 +10,24 @@ import { getHtmlForWebview } from './utils';
 import { webviewOptions } from './constants';
 
 export async function activate(context: vscode.ExtensionContext) {
-  const panel = vscode.window.createWebviewPanel(
-    'column-one',
-    'Example WebView Panel',
-    vscode.ViewColumn.One,
-    webviewOptions
-  );
-  panel.webview.html = await getHtmlForWebview(panel.webview, context.extensionUri);
-  
-  const panelProviders: WebviewProvider[] = [
-    TodoAppPanel.register(context, "panel1"),
-    TodoAppPanel.register(context, "panel2"),
-    // @ts-expect-error
-    panel
-  ];
+    const panel = vscode.window.createWebviewPanel(
+        'column-one',
+        'Example WebView Panel',
+        vscode.ViewColumn.One,
+        webviewOptions
+    );
+    panel.webview.html = await getHtmlForWebview(panel.webview, context.extensionUri);
 
-  const ch = new Channel('vscode-awesome-ux');
-  await ch.registerPromise(panelProviders);
+    const panelProviders: WebviewProvider[] = [
+        TodoAppPanel.register(context, "panel1"),
+        TodoAppPanel.register(context, "panel2"),
+        // @ts-expect-error
+        panel
+    ];
+
+    const ch = new Channel('vscode-awesome-ux');
+    await ch.registerPromise(panelProviders);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {};
+export function deactivate() { };
--- src/webview/todoApp.ts
+++ src/webview/todoApp.ts
@@ -1,10 +1,10 @@
 import {
-  window,
-  Uri,
-  Webview,
-  WebviewView,
-  WebviewViewProvider,
-  ExtensionContext
+    window,
+    Uri,
+    Webview,
+    WebviewView,
+    WebviewViewProvider,
+    ExtensionContext
 } from 'vscode';
 import { Subject } from "rxjs";
 import type { WebviewProvider } from "tangle";
@@ -13,31 +13,31 @@ import { webviewOptions } from '../constants';
 import { getHtmlForWebview } from '../utils';
 
 export default class TodoAppPanel implements WebviewViewProvider, WebviewProvider {
-  private _webview = new Subject<Webview>();
+    private _webview = new Subject<Webview>();
 
-  constructor(
-    private readonly _context: ExtensionContext,
-    public readonly identifier: string
-  ) {}
+    constructor(
+        private readonly _context: ExtensionContext,
+        public readonly identifier: string
+    ) { }
 
-  async resolveWebviewView (webviewView: WebviewView): Promise<void> {
-    webviewView.webview.html = await getHtmlForWebview(webviewView.webview, this._context.extensionUri);
-    webviewView.webview.options = {
-      ...webviewOptions,
-      localResourceRoots: [this._context.extensionUri],
-    };
-    this._webview.next(webviewView.webview);
-  }
+    async resolveWebviewView(webviewView: WebviewView): Promise<void> {
+        webviewView.webview.html = await getHtmlForWebview(webviewView.webview, this._context.extensionUri);
+        webviewView.webview.options = {
+            ...webviewOptions,
+            localResourceRoots: [this._context.extensionUri],
+        };
+        this._webview.next(webviewView.webview);
+    }
 
-  public static register(context: ExtensionContext, identifier: string) {
-    const panelProvider = new TodoAppPanel(context, identifier);
-    context.subscriptions.push(
-      window.registerWebviewViewProvider(identifier, panelProvider)
-    );
-    return panelProvider;
-  }
+    public static register(context: ExtensionContext, identifier: string) {
+        const panelProvider = new TodoAppPanel(context, identifier);
+        context.subscriptions.push(
+            window.registerWebviewViewProvider(identifier, panelProvider)
+        );
+        return panelProvider;
+    }
 
-  public get webview() {
-    return this._webview.asObservable();
-  }
+    public get webview() {
+        return this._webview.asObservable();
+    }
 }
