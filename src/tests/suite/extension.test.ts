2ca96cc1c90205449122bccbe6742203ff89ae46
Christian Bromann <git@bromann.dev>
2021-12-06 14:24:17 +0100

    minor cleanups

--- src/tests/suite/extension.test.ts
+++ src/tests/suite/extension.test.ts
@@ -1,18 +1,6 @@
-import * as assert from 'assert';
-import { after } from 'mocha';
+import assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from 'vscode';
-import { activate, deactivate } from '../../extension';
-
-suite('Extension Test Suite', () => {
-    after(() => {
-        vscode.window.showInformationMessage('All tests done!');
-    });
-
-    vscode.window.showInformationMessage('Start all tests.');
-
+suite('Extension Test Suite', function () {
     test('Sample test', () => {
         assert.strictEqual(-1, [1, 2, 3].indexOf(5));
         assert.strictEqual(1, [1, 2, 3].indexOf(0));
--- src/tests/suite/index.ts
+++ src/tests/suite/index.ts
@@ -1,32 +1,20 @@
 import path from 'path';
-import { promisify } from 'util';
 import Mocha from 'mocha';
-import globCb from 'glob';
+import glob from 'glob';
 
-const glob = promisify(globCb);
-
-export async function run(): Promise<void> {
+export function run (testsRoot: string, cb: Function) {
     // Create the mocha test
     const mocha = new Mocha({
         ui: 'tdd',
         color: true
     });
 
-    const testsRoot = path.resolve(__dirname, '..');
-    const files = await glob('**/**.test.js', { cwd: testsRoot });
+    const files = glob.sync('**/**.test.js', { cwd: path.dirname(testsRoot) });
 
     // Add files to the test suite
     console.log(`Running ${files.length} test files`);
-    files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+    files.forEach(f => mocha.addFile(path.resolve(path.dirname(testsRoot), f)));
 
-    return new Promise(async (resolve, reject) => {
-        // Run the mocha test
-        mocha.run(failures => {
-            if (failures > 0) {
-                return reject(new Error(`${failures} tests failed.`));
-            }
-
-            resolve();
-        });
-    });
+    // Run the mocha test
+    mocha.run(failures => cb(null, failures));
 }
